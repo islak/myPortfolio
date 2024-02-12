8eecde0372fd428fcf5b2ba909aab63155042290
Hersh <kalsi.hersh@gmail.com>
2024-02-12 14:18:21 -0800

    description

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,14 +6,14 @@ const Projects = forwardRef((props, ref) => {
       <h2>Projects</h2>
       <div className="project-item">
         <h3>Yelp Model</h3>
-        <p>A Yelp model project description.</p>
+        <p> Developed a sentiment analysis model for Yelp reviews using PyTorch and Hugging Face Transformers library. Utilized bert transformer model for pre-training, tokenization, and sentiment classification. Interpreted model by visualizing positive sentiments with Word Clouds using Matplotlib Technologies..</p>
         <a href="https://github.com/islak/yelp-model" target="_blank" rel="noopener noreferrer">
           Source Code
         </a>
       </div>
       <div className="project-item">
         <h3>Vigenere Cipher Cryptanalysis</h3>
-        <p>A Vigenere Cipher cryptanalysis project description.</p>
+        <p>A Python script is a cryptanalysis tool designed to aid in breaking XOR-based encryption. The script assumes that the text is encrypted using a repeating-key XOR cipher.</p>
         <a href="https://github.com/islak/vigenere-cipher" target="_blank" rel="noopener noreferrer">
         Source Code
         </a>
